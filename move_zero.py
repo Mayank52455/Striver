a4835dce6572f0bab7e9068c645e187cec8e34aa
Mayank52455 <163654388+Mayank52455@users.noreply.github.com>
2026-01-12 10:49:47 +1100

    Create move_zero.py

---
+++ move_zero.py
@@ -0,0 +1,10 @@
+class Solution:
+    def movezero(self, nums):
+        j = 0  # position for next non-zero
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
+
+        return nums
